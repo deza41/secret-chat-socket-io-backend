50bd142895445c1a732b5f0c15013d8c68b65e6c
deza41 <deza41@hotmail.com>
2021-07-29 14:07:15 +1000

    heroku node

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.post("/chatlist", async (req,res) =>{
 
 app.get("/chatlist", async (req,res) =>{
     try{
-        //  const newTodo = await pool.query("SELECT * FROM msg_log")
-         const newTodo = await client.query(" SELECT t.*FROM(SELECT * FROM msg_log ORDER BY msg_id DESC LIMIT 5 ) t ORDER BY t.msg_id ASC")
+         const newTodo = await client.query("SELECT * FROM msg_log")
+        //  const newTodo = await client.query(" SELECT t.*FROM(SELECT * FROM msg_log ORDER BY msg_id DESC LIMIT 5 ) t ORDER BY t.msg_id ASC")
          res.json(newTodo);
     } catch (err){
         console.error(err.message);
